15263d69b66ae49d70b175ea9a16bc066c56c7fa
joseph <josephyim224@gmail.com>
2020-09-26 16:32:30 +0800

    cluster: done

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -4,3 +4,4 @@ import codeitsuisse.routes.square
 import codeitsuisse.routes.revisitgeometry
 import codeitsuisse.routes.olympiad_of_babylon
 import codeitsuisse.routes.inventory_management
+import codeitsuisse.routes.cluster
