1f5643df3680f9245c137d37a7595afbeee73fa1
joseph <josephyim224@gmail.com>
2020-09-26 23:59:29 +0800

    inventory-management: done~

--- codeitsuisse/routes/inventory_management.py
+++ codeitsuisse/routes/inventory_management.py
@@ -9,143 +9,162 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 
+def solve(search_item, item):
+    # simplfy
+    def simplify(search_item, item_with_symbol):
+        symbols = dict()
+
+        # dummy start
+        symbol = str(len(symbols.keys()))
+        symbols[symbol] = ""
+        search_item_with_symbol = symbol
+        item_with_symbol = symbol + item_with_symbol
+
+        common_string = search_item[0]
+        search_item = search_item[1:] if len(search_item) > 1 else ""
+        while len(search_item) or len(common_string):
+            # print(common_string, "|", search_item, "|", search_item_with_symbol, "|", item_with_symbol)
+            if len(search_item) and item_with_symbol.find(common_string + search_item[0]) != -1:
+                common_string += search_item[0]
+                search_item = search_item[1:] if len(search_item) > 1 else ""
+                continue
+
+            if len(common_string) > 2:
+                symbol = str(len(symbols.keys()))
+                symbols[symbol] = common_string
+                search_item_with_symbol += symbol
+                item_with_symbol = item_with_symbol.replace(common_string, symbol)
+                common_string = search_item[0] if len(search_item) > 0 else ""
+                search_item = search_item[1:] if len(search_item) > 1 else ""
+                continue
+
+            search_item_with_symbol += common_string
+            common_string = search_item[0] if len(search_item) > 0 else ""
+            search_item = search_item[1:] if len(search_item) > 1 else ""
+
+        # dummy end
+        symbol = str(len(symbols.keys()))
+        symbols[symbol] = ""
+        search_item_with_symbol += symbol
+        item_with_symbol += symbol
+
+        # print("search_item_with_symbol", search_item_with_symbol)
+        # print("item_with_symbol", item_with_symbol)
+        # print("symbols", symbols)
+
+        return search_item_with_symbol, item_with_symbol, symbols
+
+    def compare(search_item, item):
+        basket = [(search_item, item, "", 0)]
+        found = list()
+        while (len(basket)):
+            search_item, item, result, no_operation = basket.pop()
+            if len(search_item) == 0 and len(item) == 0:
+                found.append([result, no_operation])
+                continue
+            if len(search_item) > 0 and len(item) == 0:
+                for c in search_item:
+                    result += '-' + c
+                    no_operation += 1
+                found.append([result, no_operation])
+                continue
+            if len(search_item) == 0 and len(item) > 0:
+                for c in item:
+                    result += '+' + c
+                    no_operation += 1
+                found.append([result, no_operation])
+                continue
+            if search_item[0].lower() == item[0].lower():
+                basket.append((search_item[1:], item[1:], result+item[0], no_operation))
+                continue
+            
+            # insertion
+            basket.append((search_item, item[1:], result + '+' + item[0], no_operation+1))
+
+            # substitution
+            basket.append((search_item[1:], item[1:], result + item[0], no_operation+1))
+
+            # remove
+            basket.append((search_item[1:], item, result + '-' + search_item[0], no_operation+1))
+
+        found.sort(key=lambda x: x[0], reverse=True)
+        found.sort(key=lambda x: x[1])
+        # print("found", found)
+        return found[0]
+
+    search_item_with_symbol, item_with_symbol, symbols = simplify(search_item, item)
+
+    item_out_with_symbol = ""
+    no_operation = 0
+    for i in range(len(symbols.keys()) - 1):
+        search_item_left = search_item_with_symbol.find(str(i)) + 1
+        search_item_right = search_item_with_symbol.find(str(i+1))
+        sub_search_item = search_item_with_symbol[search_item_left:search_item_right]
+
+        item_left = item_with_symbol.find(str(i)) + 1
+        item_right = item_with_symbol.find(str(i+1))
+        sub_item = item_with_symbol[item_left:item_right]
+
+        # print("sub_search_item", sub_search_item)
+        # print("sub_item", sub_item)
+
+        # last character is intended lost
+        found = compare(sub_search_item, sub_item)
+        item_out_with_symbol += str(i) + found[0]
+        no_operation += found[1]
+        # print(found)
+
+    # print(search_item_with_symbol)
+    # print(item_with_symbol)
+    # print(item_out_with_symbol)
+
+    item_out = ""
+    for c in item_out_with_symbol:
+        if c.isdigit():
+            item_out += symbols[c]
+        else:
+            item_out += c
+    # print(item_out, item, no_operation)
+    return item_out, item, no_operation
+
+# search_item = "atrgdzoqswycqqwqfpidvexvewjjesgeupawmruvxorcnicdmri"
+# item = "atrgzoqsycvfwvqfpidvexavewrjjesgeupamruvxornicdmri"
+
+# item_out, item, no_operation = solve(search_item, item)
+
 @app.route('/inventory-management', methods=['POST'])
 def inventory_management():
     data = request.get_json()
-
-    found = list()
-    def diverge(remaining_search_item, remaining_item, original_remaining_item, constructed="", no_operation=0):
-        # print(remaining_search_item, remaining_item, constructed, no_operation)
-
-        if no_operation > found[0][0]:
-            return
-
-        if len(remaining_search_item) == 0 and len(remaining_item) == 0:
-            # terminating case
-            found.append([remaining_search_item, remaining_item, original_remaining_item, constructed, no_operation])
-            found[0][0] = no_operation if found[0][0] > no_operation else found[0][0]
-        
-        elif len(remaining_search_item) > 0 and len(remaining_item) == 0:
-            # only remaining_search_item left
-            diverge(
-                remaining_search_item[1:], 
-                remaining_item, 
-                original_remaining_item, 
-                constructed + "-" + remaining_search_item[0], 
-                no_operation + 1
-            )
-            
-        elif len(remaining_search_item) == 0 and len(remaining_item) > 0:
-            # only remaining_item left
-            diverge(
-                remaining_search_item, 
-                remaining_item[1:], 
-                original_remaining_item, 
-                constructed + "+" + remaining_item[0],
-                no_operation + 1
-            )
-            
-        elif remaining_item[0].lower() == remaining_search_item[0].lower():
-            # exact match
-            # print("3", remaining_search_item, remaining_item, constructed, no_operation)
-            diverge(
-                remaining_search_item[1:], 
-                remaining_item[1:], 
-                original_remaining_item, 
-                constructed + remaining_item[0],
-                no_operation
-            )
-        
-        elif len(remaining_item) > 1 and remaining_item[1].lower() == remaining_search_item[0].lower():
-            # try insertion
-            # print("4.1", remaining_search_item, remaining_item[1:], constructed + '+' + remaining_item[0], no_operation+1)
-            diverge(
-                remaining_search_item,
-                remaining_item[1:],
-                original_remaining_item, 
-                constructed + '+' + remaining_item[0],
-                no_operation + 1
-            )
-
-        elif len(remaining_search_item) > 1 and remaining_item[0].lower() == remaining_search_item[1].lower():
-            # try deletion
-            # print("6", remaining_search_item[1:], remaining_item, constructed + "-" + remaining_search_item[0], no_operation+1)
-            diverge(
-                remaining_search_item[1:],
-                remaining_item,
-                original_remaining_item, 
-                constructed + "-" + remaining_search_item[0],
-                no_operation + 1
-            )
-
-        elif len(remaining_search_item) > 1 and len(remaining_item) > 1 and remaining_item[1].lower() == remaining_search_item[1].lower():
-            # try substitution
-            # print("5.1", remaining_search_item[1:], remaining_item[1:], constructed + remaining_item[0], no_operation+1)
-            diverge(
-                remaining_search_item[1:],
-                remaining_item[1:],
-                original_remaining_item, 
-                constructed + remaining_item[0],
-                no_operation + 1
-            )
-
-        else:
-            # try insertion
-            # print("4.1", remaining_search_item, remaining_item[1:], constructed + '+' + remaining_item[0], no_operation+1)
-            # diverge(
-            #     remaining_search_item,
-            #     remaining_item[1:],
-            #     original_remaining_item, 
-            #     constructed + '+' + remaining_item[0],
-            #     no_operation + 1
-            # )
-
-            # # try substitution
-            # # print("5", remaining_search_item[1:], remaining_item[1:], constructed + remaining_item[0], no_operation+1)
-            # diverge(
-            #     remaining_search_item[1:],
-            #     remaining_item[1:],
-            #     original_remaining_item, 
-            #     constructed + remaining_item[0],
-            #     no_operation + 1
-            # )
-
-            # # try deletion
-            # # print("6", remaining_search_item[1:], remaining_item, constructed + "-" + remaining_search_item[0], no_operation+1)
-            diverge(
-                remaining_search_item[1:],
-                remaining_item,
-                original_remaining_item, 
-                constructed + "-" + remaining_search_item[0],
-                no_operation + 1
-            )
 
     with open("in" + '.json', 'w') as outfile:
         json.dump(data, outfile)
 
     outAll = list()
-    for input in data:
+    for input in data:        
         result_found = list()
         search_item = input["searchItemName"]
 
         with open(search_item + '.json', 'w') as outfile:
-            json.dump(data, outfile)
+            json.dump(input, outfile)
 
         for item in input["items"]:
-            found = [[math.inf]] # current min operation
-            diverge(search_item, item, item)
-            found = found[1:]
-            found.sort(key=lambda x: x[3])
-            result_found.append(found[0][2:])
-        result_found.sort(key=lambda x: x[0]) # sort by alphatic
-        result_found.sort(key=lambda x: x[2]) # sort by operation
-        result_found = [result[1] for result in result_found]
+            item_out, item, no_operation = solve(search_item, item)
+            result_found.append([item_out, item, no_operation])
+
+        result_found.sort(key=lambda x: x[1], reverse=False) # sort by item
+        result_found.sort(key=lambda x: x[2], reverse=False) # sort by operation
+        original_item = [result[1] for result in result_found]
+        no_operation = [result[2] for result in result_found]
+        result_found = [result[0] for result in result_found]
+
         if len(result_found) > 10:
             result_found = result_found[:10]
-        outAll.append({"searchItemName": search_item, "searchResult":result_found})
+            no_operation = no_operation[:10]
+            original_item = original_item[:10]
+        outAll.append({"searchItemName": search_item, "searchResult":result_found, "no_operation": no_operation, "original_item": original_item})
 
         with open(search_item  + "_out" + '.json', 'w') as outfile:
-            json.dump({"searchItemName": search_item, "searchResult":result_found}, outfile)
+            json.dump(outAll, outfile)
 
     with open("out" + '.json', 'w') as outfile:
         json.dump(outAll, outfile)
