1ca6e1b4e039337cdddc3c92769a76452746beba
LaurenceLungo <pnng@connect.ust.hk>
2020-09-26 09:54:10 +0800

    no ; should exist in python!!

--- App.py
+++ App.py
@@ -1,22 +1,22 @@
 import logging
 import socket
-from codeitsuisse import  app
+from codeitsuisse import app
+
 logger = logging.getLogger(__name__)
+
 
 @app.route('/', methods=['GET'])
 def default_route():
-    return "Python Template";
+    return "Python Template"
 
 
 logger = logging.getLogger()
 handler = logging.StreamHandler()
 formatter = logging.Formatter(
-        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
-
-
 
 if __name__ == "__main__":
     logging.info("Starting application ...")
